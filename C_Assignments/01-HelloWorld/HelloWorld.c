70e7d83fad1caf9519d8dc14e9d68cbaf7338f88
Atharvak006 <146957412+Atharvak006@users.noreply.github.com>
2026-05-20 13:21:11 +0530

    Update HelloWorld.c

--- C_Assignments/01-HelloWorld/HelloWorld.c
+++ C_Assignments/01-HelloWorld/HelloWorld.c
@@ -6,21 +6,21 @@
 #define MY_NAME "Atharva"
 // Preprocessor Directive.
 
-enum Months
-{
-    JANUARY = 1,
-    FEBRUARY = 2,
-    MARCH = 3,
-    APRIL = 4,
-    MAY = 5,
-    JUNE = 6,
-    JULY = 7,
-    AUGUST = 8,
-    SEPTEMBER = 9,
-    OCTOBER = 10,
-    NOVEMBER = 11,
-    DECEMBER = 12
-};
+// enum Months
+// {
+//     JANUARY = 1,
+//     FEBRUARY = 2,
+//     MARCH = 3,
+//     APRIL = 4,
+//     MAY = 5,
+//     JUNE = 6,
+//     JULY = 7,
+//     AUGUST = 8,
+//     SEPTEMBER = 9,
+//     OCTOBER = 10,
+//     NOVEMBER = 11,
+//     DECEMBER = 12
+// };
 
 int main(void)
 {
